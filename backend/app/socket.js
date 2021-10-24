67f641ba007fd5c7dbcf74bf0da2faccb574a236
kyoya <m.kyoya777@gmail.com>
2021-10-24 17:50:01 +0900

    create socket

--- backend/app/socket.js
+++ backend/app/socket.js
@@ -1,5 +1,20 @@
 module.exports = (io) => {
   io.on("connection", function (socket) {
     console.log("connected");
+
+    socket.on("create-room", () => {
+      console.log("create-room");
+      data = { room_id: 123 };
+      socket.emit("create-room-response", data);
+    });
+    socket.on("enter-room", (req) => {
+      console.log(req);
+      socket.emit("enter-room-response", req);
+    });
+
+    socket.on("start-game", (req) => {
+      console.log(req);
+      socket.emit("start-game-response", req);
+    });
   });
 };
