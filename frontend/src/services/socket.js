c93df0842475bdd35b856f82a9c7f32ac614c62e
Shuhei Kayawari <dev.wowry@gmail.com>
2021-10-28 04:36:20 +0900

    socket通信を書く用のファイルを追加

---
+++ frontend/src/services/socket.js
@@ -0,0 +1,13 @@
+// import socketIOClient from "socket.io-client";
+
+// const ENDPOINT = "http://162.221.29.97:8080";
+
+// const socket = socketIOClient(ENDPOINT);
+
+// socket.on("event-name", (data) => {
+// });
+
+// socket.on("event-name", (data) => {
+// });
+
+// export socket
