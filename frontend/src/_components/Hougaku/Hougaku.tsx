5a8a994f3685604a311d44b92a6015dfaaff4d87
Shuhei Kayawari <dev.wowry@gmail.com>
2021-10-29 03:01:44 +0900

    Update Hougaku component

--- frontend/src/_components/Hougaku/Hougaku.tsx
+++ frontend/src/_components/Hougaku/Hougaku.tsx
@@ -1,41 +1,51 @@
 import React from "react";
 import classNames from "classnames";
 import "./Hougaku.scss";
+import { KazeType, DirectionType } from "../../_type";
 
 interface Props {
-  text: "東"|"西"|"北"|"南";
-  isLighting?:boolean;
-  direction?:"up" | "right" | "left" | "down";
-  device:"host"|"client";
+  text: KazeType;
+  isLighting?: boolean;
+  direction?: DirectionType;
+  device: "host" | "client";
 }
 
 const Hougaku: React.FC<Props> = ({
-    text,isLighting=false,direction="down",device
+  text,
+  isLighting = false,
+  direction = "down",
+  device,
 }) => {
-    if(direction){console.log(direction)}
+  if (direction) {
+    console.log(direction);
+  }
   const hougaku = (
-    <div className=
+    <div
+      className={classNames(
+        "Hougaku",
         {
-            classNames(
-                "Hougaku",
-                {
-                    "Hougaku--lighting":isLighting
-                },
-                {
-                    "Hougaku--host":device=="host",
-                    "Hougaku--client":device=="client"
-                },
-                {
-                    "Hougaku--up":direction=="up",
-                    "Hougaku--down":direction=="down",
-                    "Hougaku--left":direction=="left",
-                    "Hougaku--right":direction=="right",
-                }
-            )
-        }>
-        <div className={classNames("Hougaku__container",{"Hougaku__container--host":device=="host","Hougaku__container--client":device=="client"})}>
-            {text}
-        </div>
+          "Hougaku--lighting": isLighting,
+        },
+        {
+          "Hougaku--host": device == "host",
+          "Hougaku--client": device == "client",
+        },
+        {
+          "Hougaku--up": direction == "up",
+          "Hougaku--down": direction == "down",
+          "Hougaku--left": direction == "left",
+          "Hougaku--right": direction == "right",
+        }
+      )}
+    >
+      <div
+        className={classNames("Hougaku__container", {
+          "Hougaku__container--host": device == "host",
+          "Hougaku__container--client": device == "client",
+        })}
+      >
+        {text}
+      </div>
     </div>
   );
 
