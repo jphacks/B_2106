5355e49d59f0770495405ac1885375b3815465cb
= <=>
2021-10-29 03:05:37 +0900

    update formatter

--- backend/app/room.js
+++ backend/app/room.js
@@ -1,5 +1,3 @@
-/** @format */
-
 module.exports = class Room {
   constructor(takuID) {
     this.takuID = takuID;
@@ -7,14 +5,14 @@ module.exports = class Room {
   }
   //   引数として{name: "??", id: "???"}を想定
   set player(user) {
-    console.log('player');
+    console.log("player");
     if (this.players.length > 3) {
-      throw new Error('4 players already joined');
+      throw new Error("4 players already joined");
     }
     this.players.push(user);
   }
   leavePlayer(playerID) {
-    console.log('leavePlayers');
+    console.log("leavePlayers");
     this.players = this.players.filter((p) => {
       return p.id !== playerID;
     });
