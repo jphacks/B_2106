7147b5eb705120ad1c0a75ca168f72eac4cb3c10
kyoya <m.kyoya777@gmail.com>
2021-10-28 14:55:27 +0900

    add sendMessage

--- backend/app/socket.js
+++ backend/app/socket.js
@@ -1,4 +1,6 @@
-module.exports = (io) => {
+const Room = require("./room");
+
+module.exports = (io, rooms) => {
   io.on("connection", function (socket) {
     console.log("connected");
 
@@ -14,6 +16,9 @@ module.exports = (io) => {
         return;
       }
 
+      let r = new Room(socket.id);
+      rooms[roomID] = r;
+
       socket.join(roomID);
       socket.emit("create-room-response", { roomID: roomID });
     });
@@ -28,6 +33,17 @@ module.exports = (io) => {
         socket.emit("enter-room-response", { error: "no such roomID" });
         return;
       }
+
+      try {
+        const name = req.name;
+        const r = rooms[req.roomID];
+        r.player = { name: name, id: socket.id };
+      } catch (error) {
+        console.log(error);
+        socket.emit("enter-room-response", { error: error.message });
+        return;
+      }
+
       socket.join(roomID);
 
       io.in(roomID).emit("enter-room-response", { name: req["name"] || "no name", id: socket.id });
@@ -49,5 +65,59 @@ module.exports = (io) => {
     socket.on("riichi", (req) => {});
 
     socket.on("tsumo", (req) => {});
+
+    // debug用
+    socket.on("debug-show", (req) => {
+      console.log(room[req.roomID]);
+    });
+
+    socket.on("debug-send", (req) => {
+      console.log("debug-send");
+      const clients = {
+        players: [
+          {
+            "end-point": "debug-send-response",
+            arg: [0],
+          },
+          {
+            "end-point": "debug-send-response",
+            arg: [1],
+          },
+          {
+            "end-point": "debug-send-response",
+            arg: [2],
+          },
+          {
+            "end-point": "debug-send-response",
+            arg: [3],
+          },
+        ],
+        tablet: {
+          "end-point": "debug-send-response",
+          arg: [1234],
+        },
+      };
+
+      sendMessage(req["roomID"], clients);
+    });
   });
+
+  function sendMessage(roomID, clients) {
+    const room = rooms[roomID];
+    console.log(rooms);
+    if (!room) {
+      console.log("no such room");
+      console.log(`room : ${room}`);
+      return;
+    }
+
+    const tablet = clients["tablet"];
+    io.to(room.takuID).emit(tablet["end-point"], tablet["arg"]);
+
+    const players = clients["players"];
+    for (let i = 0; i < Math.min(room["players"].length, players.length); i++) {
+      const player = players[i];
+      io.to(room["players"][i]["id"]).emit(player["end-point"], player["arg"]);
+    }
+  }
 };
